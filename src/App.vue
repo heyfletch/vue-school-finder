44e6f229314129c5b9aba5be5fce83fd70855d8e
Joe Fletcher <joe@fletcherdigital.com>
2019-08-09 14:55:07 -0400

    rename Chips to Cards

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,37 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
     <v-content>
-      <HelloWorld/>
+      <v-container grid-list-md>
+        <v-layout wrap>
+          <v-flex sm12 md6 lg4>
+            <Filters />
+          </v-flex>
+          <v-flex xs12 md6 lg4>
+            <Cards />
+          </v-flex>
+          <v-flex xs12 md12 lg4>
+            <Maps />
+          </v-flex>
+        </v-layout>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Filters from "./components/Filters";
+import Cards from "./components/Cards";
+import Maps from "./components/Maps";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld,
+    Filters,
+    Cards,
+    Maps
   },
   data: () => ({
     //
-  }),
+  })
 };
 </script>
