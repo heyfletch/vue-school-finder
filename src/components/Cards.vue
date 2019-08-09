f3099bca77117e74a4654282c9eb21abcbf51416
Joe Fletcher <joe@fletcherdigital.com>
2019-08-09 15:56:41 -0400

    Create real cards

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -3,23 +3,36 @@
     <v-container class="pa-2" fluid grid-list-md>
       <v-layout column>
         <v-flex v-for="(item, i) in items" :key="i">
-          <v-card :color="item.color">
-            <v-list-item three-line>
-              <v-list-item-content class="align-self-start">
+          <v-card>
+            <v-list-item one-line>
+              <v-list-item-content class="align-self-start pb-0">
                 <v-list-item-title
-                  class="headline mb-2"
+                  class="headline mt-5"
                   v-text="item.title"
                 ></v-list-item-title>
 
-                <v-list-item-subtitle
-                  v-text="item.artist"
-                ></v-list-item-subtitle>
-              </v-list-item-content>
+                <v-layout py-1 px-1>
+                  <v-card-text
+                    class="px-0 py-3"
+                    v-text="item.description"
+                  >
+                  </v-card-text>
 
-              <v-list-item-avatar size="125" tile>
-                <v-img :src="item.src"></v-img>
-              </v-list-item-avatar>
+                  <v-list-item-avatar
+                    tile
+                    size="125"
+                    color="primary">
+                    <v-img :src="item.src"></v-img>
+                  </v-list-item-avatar>
+                </v-layout>
+              </v-list-item-content>
             </v-list-item>
+
+            <v-card-actions>
+              <v-btn text>Learn More</v-btn>
+              <v-btn text>Apply Now</v-btn>
+            </v-card-actions>
+
           </v-card>
         </v-flex>
       </v-layout>
@@ -32,16 +45,14 @@ export default {
   data: () => ({
     items: [
       {
-        color: "#efefef",
-        src: "https://cdn.vuetifyjs.com/images/cards/foster.jpg",
-        title: "Summit School K2",
-        artist: "Lorem Ipsum Dolor ..."
+        src: "https://enrollwcc.org/wp-content/uploads/2018/08/summit-public-school-tamalpais-300x169.jpg",
+        title: "Summit Public School Tamalpais",
+        description: "Summit Public Schools’ mission is to prepare a diverse student population for success in college and to be thoughtful, contributing members of society.",
       },
       {
-        color: "#efefef",
-        src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png",
-        title: "AMPS - General School",
-        artist: "AMPS is leading the way in educational leadership"
+        src: "https://enrollwcc.org/wp-content/uploads/2018/08/summit-public-school-k2-300x169.jpg",
+        title: "Summit Public School K2",
+        description: "Preparing a diverse student population for success in college and to be thoughtful, contributing members of society.",
       }
     ]
   })
