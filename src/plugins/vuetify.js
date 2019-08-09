24de09f64e7b43a893b0b453776fee043e96e454
Joe Fletcher <joe@fletcherdigital.com>
2019-08-09 14:43:00 -0400

    color scheme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,19 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
+import colors from "vuetify/lib/util/colors";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
   icons: {
-    iconfont: 'mdi',
+    iconfont: "mdi"
   },
+  theme: {
+    themes: {
+      light: {
+        primary: colors.blue.darken4,
+        secondary: colors.lightBlue.lighten2
+      }
+    }
+  }
 });
