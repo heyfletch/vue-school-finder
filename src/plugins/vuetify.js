f385911186c65be31af0bfc0b6fca4d76252b429
Joe Fletcher <joe@fletcherdigital.com>
2019-08-10 18:35:22 -0400

    change to red to test deployment

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -11,7 +11,7 @@ export default new Vuetify({
   theme: {
     themes: {
       light: {
-        primary: colors.blue.darken4,
+        primary: colors.red.darken4,
         secondary: colors.lightBlue.lighten2
       }
     }
