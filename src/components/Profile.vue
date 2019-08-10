680b4a14cba37abaf080f46b369aea832a4ed600
Joe Fletcher <joe@fletcherdigital.com>
2019-08-10 06:47:54 -0400

    placeholder image

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,38 +1,34 @@
 <template>
-  <v-card
-    class="mx-1 mt-12"
-    max-width="400"
-  >
+  <v-card class="mx-1 mt-12" max-width="400">
     <v-img
       class="white--text primary"
       height="200px"
       src="https://enrollwcc.org/wp-content/uploads/2018/08/Aspire-Richmond-California.jpg"
-      gradient="to bottom, rgba(35, 82, 144, 0.1), rgba(35, 82, 144, 0.7)"
+      gradient="to bottom, rgba(35, 82, 144, 0.4), rgba(35, 82, 144, 0.4)"
     >
-      <v-card-title class="align-end fill-height">Aspire Richmond California College Prepatory Academy</v-card-title>
+      <template v-slot:placeholder>
+        <v-layout fill-height align-center justify-center ma-0>
+          <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+        </v-layout>
+      </template>
+      <v-card-title
+        class="align-end fill-height"
+      >Aspire Richmond California College Prepatory Academy</v-card-title>
     </v-img>
 
     <v-card-text>
-      <span>Number 10</span><br>
+      <span>Number 10</span>
+      <br />
       <span class="text--primary">
-        <span>Whitehaven Beach</span><br>
+        <span>Whitehaven Beach</span>
+        <br />
         <span>Whitsunday Island, Whitsunday Islands</span>
       </span>
     </v-card-text>
 
     <v-card-actions>
-      <v-btn
-        text
-        color="orange"
-      >
-        Share
-      </v-btn>
-      <v-btn
-        text
-        color="orange"
-      >
-        Explore
-      </v-btn>
+      <v-btn text color="orange">Share</v-btn>
+      <v-btn text color="orange">Explore</v-btn>
     </v-card-actions>
   </v-card>
 </template>
