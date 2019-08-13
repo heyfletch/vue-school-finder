79e4559a4bf49b804ef7f5c55a150eec6804a025
Joe Fletcher <joe@fletcherdigital.com>
2019-08-13 10:50:16 -0400

    wire up schools to real acf data

--- src/components/SchoolsList.vue
+++ src/components/SchoolsList.vue
@@ -3,18 +3,18 @@
     <v-item-group>
       <v-container class="pa-2 pb-0" fluid grid-list-md>
         <v-layout wrap>
-          <v-flex v-for="(item, i) in items" :key="i">
+          <v-flex v-for="school in schools.nodes" :key="school.id">
             <v-item v-slot:default="{ active, toggle }">
               <v-card :color="active ? 'primary' : ''" @click="toggle" class="mb-2">
                 <v-list-item one-line>
                   <v-list-item-content class="align-self-start pb-0">
-                    <v-card-title class="headline pa-0" v-text="item.title"></v-card-title>
+                    <h4 v-html="school.title"></h4>
 
                     <v-layout pb-2 pl-1>
-                      <v-card-text class="px-0 py-3" v-text="item.description"></v-card-text>
+                      <v-card-text class="px-0 py-3" v-html="school.acfSchoolProfiles.schoolIntro"></v-card-text>
 
                       <v-list-item-avatar tile size="125" color="primary">
-                        <v-img :src="item.src"></v-img>
+                        <v-img :src="school.featuredImage.sourceUrl"></v-img>
                       </v-list-item-avatar>
                     </v-layout>
                   </v-list-item-content>
@@ -29,31 +29,69 @@
 </template>
 
 <script>
+import gql from "graphql-tag";
+
 export default {
-  data: () => ({
-    items: [
-      {
-        src:
-          "https://enrollwcc.org/wp-content/uploads/2018/08/summit-public-school-tamalpais-300x169.jpg",
-        title: "Summit Public School Tamalpais",
-        description:
-          "Our mission is to prepare a diverse student population for success in college and to be thoughtful, contributing members of society."
-      },
-      {
-        src:
-          "https://enrollwcc.org/wp-content/uploads/2018/08/Aspire-Richmond-California-300x169.jpg",
-        title: "Aspire Richmond California College Prepatory Academy",
-        description:
-          "Ensure that all young people have the knowledge and skills to thrive in college and society so that they will solve the challenges our communities face today and tomorrow."
-      },
-      {
-        src:
-          "https://enrollwcc.org/wp-content/uploads/2018/08/summit-public-school-k2-300x169.jpg",
-        title: "Summit Public School K2",
-        description:
-          "Preparing a diverse student population for success in college and to be thoughtful, contributing members of society."
+  apollo: {
+    schools: gql`
+      query getSchools {
+        schools(where: { language: EN }) {
+          nodes {
+            id
+            title
+            uri
+            featuredImage {
+              sourceUrl(size: MEDIUM_LARGE)
+            }
+            language {
+              id
+              name
+            }
+            acfSchoolProfiles {
+              schoolAbsenteeism
+              schoolAddress
+              schoolAfterHours
+              schoolAsian
+              schoolBeforeHours
+              schoolBlack
+              schoolCity
+              schoolDisadvantaged
+              schoolEnglish
+              schoolEnrollment
+              schoolGraduation
+              schoolLatino
+              schoolNeighborhood
+              schoolOther
+              schoolPhoneNumber
+              schoolPrincipal
+              schoolSbacEnglishExceeded
+              schoolSbacEnglishMet
+              schoolSbacEnglishNearlyMet
+              schoolSbacEnglishNotMet
+              schoolSbacMathExceeded
+              schoolSbacMathMet
+              schoolSbacMathNearlyMet
+              schoolSbacMathNotMet
+              schoolSpecialEducation
+              schoolSuspension
+              schoolUcCsu
+              schoolUniforms
+              schoolUrl
+              schoolWhite
+              schoolZip
+              schoolGrades
+              schoolTransportation
+              schoolHours
+              schoolIntro
+              schoolCurriculum
+              schoolCulture
+              schoolPrograms
+              schoolEnrollmentPriorities
+            }
+          }
+        }
       }
-    ]
-  })
+    ` //end GGL
+  }
 };
 </script>
