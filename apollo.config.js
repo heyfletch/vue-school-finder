e230713b72c049d9b168edb658921d0de5e99e0d
Joe Fletcher <joe@fletcherdigital.com>
2019-08-14 11:15:46 -0400

    change the name

--- apollo.config.js
+++ apollo.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   client: {
     service: {
-      name: "school-finder",
+      name: "School Finder",
       // URL to the GraphQL API
       url: "https://enrollwcc.kinsta.cloud/graphql"
     },
