6b82bb33ea329f94fba968b12a4283b4463fbddc
Joe Fletcher <joe@fletcherdigital.com>
2019-08-14 11:27:56 -0400

    disable production sourcemaps for now

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === "production" ? "/vue-school-finder/" : "/"
+    process.env.NODE_ENV === "production" ? "/vue-school-finder/" : "/",
+  productionSourceMap: false
 };
