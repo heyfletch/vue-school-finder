2be1bc4778ae776ab83d28824c22363a37c7217b
Joe Fletcher <joe@fletcherdigital.com>
2019-09-03 12:10:43 -0400

    fix chip width and remove fixed dimensions, hopefully that's ok

--- src/Hero.vue
+++ src/Hero.vue
@@ -1,82 +1,101 @@
 <template>
   <v-app class="hero-app">
     <v-chip-group class="hero-chip-group" column multiple :value="getFilters('grade')">
-      <v-chip v-for="grade in grades" :key="grade" label outlined color="primary" :value="grade" @click="toggleFilter('grade', grade)">{{ grade }}</v-chip>
+      <v-chip class="hero-chip"
+        v-for="grade in grades"
+        :key="grade"
+        label
+        outlined
+        color="primary"
+        :value="grade"
+        @click="toggleFilter('grade', grade)"
+      >{{ grade }}</v-chip>
     </v-chip-group>
   </v-app>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        grades: ['TK', 'K', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
-        appHeight: 0
+export default {
+  data() {
+    return {
+      grades: [
+        "TK", "K", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"
+      ],
+      appHeight: 0
+    };
+  },
+  computed: {
+    locale() {
+      return this.$store.state.locale;
+    }
+  },
+  mounted() {
+    // eslint-disable-next-line no-undef
+    for (let a of app) {
+      if (!a.classList.value.split(" ").includes("hero-app")) {
+        // This is the actual app
+        this.appHeight = a.offsetTop;
+      }
+    }
+  },
+  methods: {
+    hasFilter(type, value) {
+      return this.$store.getters.hasFilter(type, value);
+    },
+    getFilters(type) {
+      return this.$store.getters.getFiltersByType(type).map(v => v.value);
+    },
+    toggleFilter(type, value) {
+      if (this.hasFilter(type, value)) {
+        this.removeFilter(type, value);
+        this.scrollToFinder();
+      } else {
+        this.addFilter(type, value);
+        this.scrollToFinder();
       }
     },
-    computed: {
-      locale() {
-        return this.$store.state.locale;
-      }
+    addFilter(type, value) {
+      this.$store.dispatch("addFilter", { type, value });
     },
-    mounted() {
-      // eslint-disable-next-line no-undef
-      for (let a of app) {
-        if (!a.classList.value.split(" ").includes("hero-app")) {
-          // This is the actual app
-          this.appHeight = a.offsetTop;
-        }
-      }
+    removeFilter(type, value) {
+      this.$store.dispatch("removeFilter", { type, value });
     },
-    methods: {
-      hasFilter(type, value) {
-        return this.$store.getters.hasFilter(type, value);
-      },
-      getFilters(type) {
-        return this.$store.getters.getFiltersByType(type).map(v => v.value);
-      },
-      toggleFilter(type, value) {
-        if (this.hasFilter(type, value)) {
-          this.removeFilter(type, value);
-          this.scrollToFinder();
-        }
-        else {
-          this.addFilter(type, value);
-          this.scrollToFinder();
-        }
-      },
-      addFilter(type, value) {
-        this.$store.dispatch("addFilter", {type, value});
-      },
-      removeFilter(type, value) {
-        this.$store.dispatch("removeFilter", {type, value});
-      },
-      scrollToFinder() {
-        try {
-          window.scrollTo({
-            left: 0,
-            top: this.appHeight,
-            behavior: 'smooth'
-          });
-        }
-        catch {
-          // Options probably unsupported, do it without.
-          window.scrollTo(0, this.appHeight);
-        }
+    scrollToFinder() {
+      try {
+        window.scrollTo({
+          left: 0,
+          top: this.appHeight,
+          behavior: "smooth"
+        });
+      } catch {
+        // Options probably unsupported, do it without.
+        window.scrollTo(0, this.appHeight);
       }
     }
   }
+};
 </script>
 
 <style>
+.v-application--wrap {
+  min-height: auto;
+}
 .hero-app {
-  height: 85px;
+  /* height: 85px; */
 }
 .hero-chip-group {
-  width: 320px;
-  text-align: left;
+  /* max-width: 380px; */
 }
-p, b, h4 {
+.hero-chip-group .v-chip__content {
+  display: inline-grid;
+  margin: auto;
+  width: 20px;
+  text-align: center;
+}
+
+p,
+b,
+h4 {
   color: #205089;
 }
 </style>
