51c174722cc180a7cbf20ba4b537670475dde484
Joe Fletcher <joe@fletcherdigital.com>
2019-09-12 17:00:57 -0400

    blue apply button and first

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -55,9 +55,9 @@
               </div>
             </v-card-text>
 
-            <v-card-actions>
+            <v-card-actions class="ma-2 pt-2 pb-4">
+              <v-btn :href="locale.applyLink" @click="gaHandler('Apply Now', school.title)" flat depressed color="primary">{{ locale.applyNow }}</v-btn>
               <v-btn :href="school.link" @click="gaHandler('Learn More', school.title)" text color="primary">{{ locale.learnMore }}</v-btn>
-              <v-btn :href="locale.applyLink" @click="gaHandler('Apply Now', school.title)" text color="primary">{{ locale.applyNow }}</v-btn>
             </v-card-actions>
           </v-card>
         </v-flex>
